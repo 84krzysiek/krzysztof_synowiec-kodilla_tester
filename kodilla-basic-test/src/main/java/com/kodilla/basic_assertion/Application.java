f516366852ac6c975ec3f669dda3129dba14c46a
Krzysztof Synowiec <krzysztof.synowiec84@gmail.com>
2022-06-22 19:12:03 +0200

    Initial version

--- kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -7,19 +7,25 @@ public class Application {
         int a = 12;
         int b = 7;
         int sumResult = calculator.sum(a, b);
-        int sumResultSubstract = calculator.substract(a,b);
+        int ResultSubstract = calculator.substract(a, b);
+        int square = calculator.square(a);
         boolean correct = ResultChecker.AssertEquals(19, sumResult);
         if (correct) {
-            System.out.println("Metoda sum działa poprawnie" + a + "i" + b);
+            System.out.println("Metoda sum działa poprawnie " +  a  + " i " +  b );
         } else {
-            System.out.print("Metpda sum nie dziąła poprawnie " + a + "i" + b);
+            System.out.print("Metpda sum nie dziąła poprawnie  " + a + " i " +  b);
         }
-        boolean substractRes = ResultChecker.AssertEquals(5,sumResultSubstract);
-        if (correct){
-            System.out.println("Metoda substract działa poprawnie"+ a +"i" + b);
-        } else
-            System.out.println("Metoda substract nie działa poprawnie" + a + "i" + b);
-
-
+        boolean noCorrect = ResultChecker.AssertEquals(6, ResultSubstract);
+        if (noCorrect) {
+            System.out.println("Metoda substract działa poprawnie" + a + " i " + b);
+        } else {
+            System.out.println("Metoda substract nie działa poprawnie " +   a   + "  i  " +  b);
+        }
+        boolean square1 = ResultChecker.AssertEquals(144, square);
+        if (square1) {
+            System.out.print("Metoda podnoszenia do kwadratu działa poprawnie "  +  a  +  "  ");
+        } else {
+            System.out.println("Metoda podnoszenia do kwadratu nie działa poprawnie");
+        }
     }
 }
--- kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/ResultChecker.java
+++ kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/ResultChecker.java
@@ -3,5 +3,6 @@ package com.kodilla.basic_assertion;
 public class ResultChecker {
     public static boolean AssertEquals(int expected, int actual){
         return expected == actual;
+
     }
 }
