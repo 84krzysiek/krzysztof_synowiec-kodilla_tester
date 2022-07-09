56b0c317282a129c76a1b82ad8d4638d96f9ce0f
Krzysztof Synowiec <krzysztof.synowiec84@gmail.com>
2022-07-09 10:56:27 +0200

    Initial version

--- kodilla-basic-test/src/main/java/com/kodilla/bank/homework/BankTestSuiteTest.java
+++ kodilla-basic-test/src/main/java/com/kodilla/bank/homework/BankTestSuiteTest.java
@@ -10,20 +10,20 @@ class BankTestSuiteTest {
     void shouldCalculatedSaldo() {
         Bank saldoAllAtm = new Bank();
         saldoAllAtm.allAtmSaldo();
-        assertEquals(100,saldoAllAtm.allAtmSaldo());
+        Assertions.assertEquals(100,saldoAllAtm.allAtmSaldo());
     }
 
     @Test
     void shouldCalculateAverageCashOut() {
         Bank averageOut = new Bank();
         averageOut.averageCashOut();
-        assertEquals(100,averageOut.averageCashOut());
+        Assertions.assertEquals(100,averageOut.averageCashOut());
 
     }
 
     @Test
     public void shouldCalculateAverage() {
         Bank averageIn = new Bank();
-        assertEquals(50, averageIn.averageCashIn(0), 0.01);
+        Assertions.assertEquals(50, averageIn.averageCashIn(0), 0.01);
     }
 }
